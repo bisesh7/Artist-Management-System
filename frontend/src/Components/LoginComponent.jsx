6818efc08b7818563501570da0dd28fb29b42a40
Bisesh Shakya <bisesh.shakya@gmail.com>
2026-03-11 12:05:51 +0545

    style: login box initial design

--- frontend/src/Components/LoginComponent.jsx
+++ frontend/src/Components/LoginComponent.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-
+import { Button, Form } from "react-bootstrap";
 const LoginComponent = () => {
   return (
     <div>
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Group className="mb-2">
+          <Form.Control type="email" placeholder="Email Address" />
         </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <div class="d-grid gap-2">
+          <Button variant="primary">Login</Button>
+        </div>
+        <div className="d-flex justify-content-end">
+          <span className="ml-auto">Forgot Password</span>
+        </div>
       </Form>
     </div>
   );
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,8 +1,40 @@
-import React from "react";
 import LoginComponent from "../Components/LoginComponent";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { BsPersonCircle } from "react-icons/bs";
+import styles from "../Styles/login.module.scss";
+import classNames from "classnames";
 
 const Login = () => {
-  <LoginComponent />;
+  return (
+    <Row className="vh-100">
+      <Col className="d-flex justify-content-center align-items-center">
+        <div
+          className={classNames(styles.loginBox, {
+            "align-center": true,
+          })}
+        >
+          <div className="d-flex">
+            <h1>AMS</h1>
+            <BsPersonCircle size={32} className="mt-2" />
+          </div>
+
+          <span>Artist Management System</span>
+          <LoginComponent />
+        </div>
+      </Col>
+      <Col>
+        <Container>
+          <h2>New Admin Registration</h2>
+          <span>
+            Access the powerful tools for artist and song collection management.
+            Complete the registration form.
+          </span>{" "}
+          <br />
+          <Button>Register New Account</Button>
+        </Container>
+      </Col>
+    </Row>
+  );
 };
 
 export default Login;
