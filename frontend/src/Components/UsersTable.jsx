7e3339709b7fcab455d3f837ec1cd4563053a495
Bisesh Shakya <bisesh.shakya@gmail.com>
2026-03-13 19:54:07 +0545

    feat: create users table

---
+++ frontend/src/Components/UsersTable.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import Table from "react-bootstrap/Table";
+import ResponsivePagination from "react-responsive-pagination";
+
+function TableComponent() {
+  const [currentPage, setCurrentPage] = useState(8);
+  const totalPages = 20;
+
+  return (
+    <>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Date Of Birth</th>
+            <th>Gender</th>
+            <th>Address</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Bisesh</td>
+            <td>Shakya</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+      <ResponsivePagination
+        current={currentPage}
+        total={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </>
+  );
+}
+
+export default TableComponent;
--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,18 +1,19 @@
-import { Col, Nav, Row } from "react-bootstrap";
-import { FaPencilAlt, FaUsers } from "react-icons/fa";
+import { Button, Col, Form, InputGroup, Nav, Row } from "react-bootstrap";
+import { FaPencilAlt, FaSearch, FaUsers } from "react-icons/fa";
 import { MdLogout, MdOutlineDashboard } from "react-icons/md";
 import NavbarComponent from "../Components/NavbarComponent";
+import { FaPlus } from "react-icons/fa6";
+import UsersTable from "../Components/UsersTable";
 
 const Dashboard = () => {
   return (
     <Row>
       <Col md="3">
-        <h4 className="text-center">AMS</h4>
-        <span className="text-center"> Artist Management System</span>
+        <h4 className="text-center mt-3">AMS</h4>
+        <div className="d-flex justify-content-center">
+          <span> Artist Management System</span>
+        </div>
         <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link href="/home">
-            <MdOutlineDashboard /> Dashboard
-          </Nav.Link>
           <Nav.Link eventKey="/users">
             <FaUsers />
             &nbsp;Users
@@ -30,6 +31,30 @@ const Dashboard = () => {
 
       <Col>
         <NavbarComponent />
+        <h1>User Management</h1>
+        <span>List of users:</span>
+
+        <div className="me-3">
+          <Row>
+            <Col md="">
+              <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">
+                  <FaSearch />
+                </InputGroup.Text>
+                <Form.Control placeholder="Search" />
+              </InputGroup>
+            </Col>
+            <Col className="d-flex justify-content-end">
+              <div>
+                <Button>
+                  <FaPlus /> Add User{" "}
+                </Button>
+              </div>
+            </Col>
+          </Row>
+
+          <UsersTable />
+        </div>
       </Col>
     </Row>
   );
